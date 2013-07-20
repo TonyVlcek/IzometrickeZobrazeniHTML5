038a060e4c4f5b7f748f592107d501ed1886e48e
TonnyVlcek <vlcek.a@email.cz>
2013-07-20 21:52:50 +0200

    Test technologií
    První pokusy o vytvoření "izometrického čtverečku.

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+	<line x1="0" y1="16" x2="31" y2="0" style="stroke:rgb(255,0,0); stroke-width:1" />
+	<line x1="0" y1="16" x2="31" y2="32" style="stroke:rgb(255,0,0); stroke-width:1" />
+	<line x1="31" y1="32" x2="64" y2="16" style="stroke:rgb(255,0,0); stroke-width:1" />
+	<line x1="64" y1="16" x2="31" y2="0" style="stroke:rgb(255,0,0); stroke-width:1" />
+</svg>
+<svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+	 <polygon points="0,16 31,0 0,16 31,32 64,16 31,0" style="fill: orange;"/>
+	 <polygon points="0,16 31,0 0,16 31,32 64,16 31,0" style="fill: orange;"/>
+</svg>
+
+</body>
+</html>
