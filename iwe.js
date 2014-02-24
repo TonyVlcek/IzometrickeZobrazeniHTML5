cd6c82e866c91a5f56e0267eb7ae9248833b6374
Tonny <vlcek.a@email.cz>
2014-02-24 19:46:01 +0100

    Finální verze zdrojových kódů pro publikování SOČ
    Stejné zdrojové kódy, které sjou aktuální s tímto commitem, bdu vypáleny
    na CD a přiloženy k mé práci

--- iwe.js
+++ iwe.js
@@ -16,45 +16,39 @@ Antonín Vlček
 Gymnázium Brno-Řečkovice 
 RedHat | JCMM | JMK
 */
-var iwe = (function () { 
+var iwe = (function () {
     /*private variables*/
-    var tile = {width: 52, height: 30};
     var imagesLoaded = false;
-    var aImages = [];
+    var isVisibleOffset = 50; //only for betatesting 
     /*functions*/
     function $(id) {
         return document.getElementById(id);
     }
     /*main object*/
     var iwe = {
-        Tile: function (x, y, sprite) {
-            this.x = x;
-            this.y = y;
-            this.sprite = sprite;
-        },
-        Tiles: { //here will be all your tile prototypes
+        tile: {
+            width: null,
+            height: null
         },
         Canvas: {
             ctx: null
         },
         Map: {
-            offset: {x: 0, y: 0},
-            model: []
+            offset: {x: 0, y: 0}
         },
-        Model: {
-             
-        },
+        Model: [],
         isVisible: function (x, y) {
-            if ((x + iwe.Map.offset.x + tile.width - 100 >= iwe.Map.offset.x) && (x + iwe.Map.offset.x <= iwe.Map.offset.x + iwe.Canvas.width - 100) && (y + iwe.Map.offset.y >= iwe.Map.offset.y) && (y + iwe.Map.offset.y <= iwe.Map.offset.y + iwe.Canvas.height - 100)) {
+            if ((x + iwe.Map.offset.x + iwe.tile.width - isVisibleOffset >= iwe.Map.offset.x) && (x + iwe.Map.offset.x <= iwe.Map.offset.x + iwe.Canvas.width - isVisibleOffset) && (y + iwe.Map.offset.y >= iwe.Map.offset.y) && (y + iwe.Map.offset.y <= iwe.Map.offset.y + iwe.Canvas.height - isVisibleOffset)) {
                 return true;
-            } else {
+            }
+            else{
                 return false;
             }
         },
         images: [],
         loadImages: function (imagesDir, array) {
-            var images, remaining = array.length;
-            for (var i = 0; i < array.length; i++){
+            var images, remaining = array.length, i;
+            for (i = 0; i < array.length; i++){
                 iwe.images[i] = new Image(); 
                 iwe.images[i].src = imagesDir+array[i];
                 remaining --;
@@ -63,67 +57,84 @@ var iwe = (function () {
         },
         TRANSFORM: {
             toIso: function (x,y){
-                var isoX = ((x-y)*(tile.width/2))+iwe.Map.offset.x; 
-                var isoY = (((x+y))*(tile.height/2))+iwe.Map.offset.y;
+                var isoX = ((x-y)* (iwe.tile.width/2))+iwe.Map.offset.x; 
+                var isoY = (((x+y))*(iwe.tile.height/2))+iwe.Map.offset.y;
                 return {x: isoX, y: isoY};
             },
             toModel: function(x,y){
                 x -= iwe.Map.offset.x;
                 y -= iwe.Map.offset.y;
-                var modelX = Math.round(((x/(tile.width/2)) + (y/(tile.height/2)))/2 - 1);
-                var modelY = Math.round(((y/(tile.height/2))- (x/(tile.width/2)))/2);
+                var modelX = Math.round(((x/(iwe.tile.width/2)) + (y/(iwe.tile.height/2)))/2 - 1);
+                var modelY = Math.round(((y/(iwe.tile.height/2))- (x/(iwe.tile.width/2)))/2);
                 return {x: modelX, y: modelY};
             }
        },
        DRAW: {
            image: function (url,x,y){
                 if(iwe.isVisible(x,y)){
-                    iwe.Canvas.ctx.drawImage(url, x, y);   
+                    iwe.Canvas.ctx.drawImage(url, x, y);  
+                    return true;
+                }else {
+                    return false;
                 }
            },
            tile: function (tile){
                 var isoCoor = iwe.TRANSFORM.toIso(tile.x, tile.y);
-                isoCoor.x -= tile.offset.x;
-                isoCoor.y -= tile.offset.y;
+                isoCoor.x -= tile.offsetX;
+                isoCoor.y -= tile.offsetY;
                 iwe.DRAW.image(iwe.images[tile.spriteId], isoCoor.x, isoCoor.y);
            },
            all: function (){
-                for(var y=0; y < iwe.Map.model.length; y++){
-                    for(var x=0; x < iwe.Map.model[y].length; x++){ 
-                        var tileToDraw = iwe.Tiles['t'+iwe.Map.model[y][x]];
-                        tileToDraw.x = y;
-                        tileToDraw.y = x;
-                        iwe.DRAW.tile(tileToDraw);
+                for(var i=0; i < iwe.Model.length; i++){
+                    for(var j=0; j < iwe.Model[i].length; j++){ 
+                        iwe.DRAW.tile(iwe.Model[i][j]);
                     }
                 }
             }
        },
        CLEAR: function(){
             iwe.Canvas.ctx.clearRect(0,0,iwe.Canvas.width,iwe.Canvas.height);
-       },
-       COLISION: function(x, y){
-            if(iwe.Tiles['t'+iwe.Map.model[x][y]].walkable){
-                return false;
-            }else{
-                return true;
-            } 
-       },
+       },    
        getTile: function(x,y){
-           return iwe.Map.model[x][y];
+           return iwe.Model[x][y];
        },
-       loadMap: function (array,offsetX,offsetY){
-            iwe.Map.model = array; 
-            iwe.Map.offset.x = offsetX;
-            iwe.Map.offset.y = offsetY;
+       fullScreen: function(){
+            document.body.style.margin = "0";
+            document.body.style.padding = "0";
+            document.body.style.height = window.innerHeight;
+            document.body.style.width = window.innerWidth;
+            document.body.style.overflow = "hidden";
+            iwe.Canvas.height = window.innerHeight;
+            iwe.Canvas.width = window.innerWidth;
+            iwe.Canvas.style = 'border: 0;';
+            window.scrollTo(0, 1);
+       },
+       fillModel: function (map, tiles){
+         for (var i = 0; i < map.length; i++) {
+                iwe.Model.push([]);
+                for (var j = 0; j < map[i].length; j++) {
+                    var tile = new tiles['t'+map[i][j]](i,j);
+                    iwe.Model[i].push(tile);
+                }
+            }  
        },
        INIT: function(init) {
             iwe.Canvas = $(init.canvas);
             iwe.Canvas.ctx = iwe.Canvas.getContext("2d");
-            iwe.loadImages(init.imagesDir, init.images);
-            iwe.loadMap (init.map,init.startMapAt.x, init.startMapAt.y);  
-        }
+            //iwe.loadImages(init.imagesDir, init.images);
+            iwe.tile.width = init.tile.width;
+            iwe.tile.height = init.tile.height;
+            iwe.Map.offset = init.startAt;
+            //iwe.loadMap (init.map,init.startMapAt.x, init.startMapAt.y);  
+            if(init.fullScreen){
+                iwe.fullScreen();
+            }
+       }
    };
     return iwe;
 }());
+
+
+
 
 
